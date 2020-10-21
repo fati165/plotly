68db165a0aedc2cc857bd6b882f03f8b6c488bd9
fati165 <gdonato38@gmail.com>
2020-10-21 15:01:24 -0700

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -11,7 +11,8 @@ function bacteria(person){
             var sample_values = otu[0].sample_values;
             var otu_ids= otu[0].otu_ids;
             var otu_labels= otu[0].otu_labels;
-
+            
+            console.log(otu_labels)
             var trace = {
                 x: otu_ids,
                 y: sample_values,
@@ -23,15 +24,42 @@ function bacteria(person){
 
             // 7. Define our plot layout
             var layout = {
-            title: "Bacteria",
-            xaxis: { title: "City" },
-            yaxis: { title: "2017 Population"}
+            title: "Belly Button Bacteria",
+            xaxis: { title: "otu" },
+            yaxis: { title: "values"}
             };
 
-            // 8. Plot the chart to a div tag with id "bar-plot"
+            // 8. Plot the chart to a div tag with id "bar"
             Plotly.newPlot("bar", data, layout);
 //metadata
 //input.metadata for metadata
+//do plots within the function
+            //for the bubble
+            var trace2= { 
+            x: otu_ids,
+            y: sample_values,
+            marker: {
+                size: sample_values,
+                color: otu_ids
+
+            },
+            text: otu_labels,
+            type: "bubble"
+            };
+
+            var layout2 = {
+                title: "Belly Button Bacteria",
+                xaxis: { title: "otu" },
+                yaxis: { title: "values"}
+                
+            };
+            var data2 = [trace2];
+ 
+            Plotly.newPlot("bubble", data2, layout2);
+
+
+
+
               
         }
     )
