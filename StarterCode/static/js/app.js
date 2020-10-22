73f6e1db79759f69513408b3adc9a18fb4a10db5
fati165 <gdonato38@gmail.com>
2020-10-22 13:27:42 -0700

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -13,7 +13,7 @@ function bacteria(person){
             var sample_values = otu[0].sample_values;
             var otu_ids= otu[0].otu_ids;
             var otu_labels= otu[0].otu_labels;
-            console.log(otu_labels)
+            console.log(otu_labels);
             //plot bar
             var trace = {
                 x: otu_ids,
@@ -58,31 +58,29 @@ function bacteria(person){
  
             Plotly.newPlot("bubble", data2, layout2);
 
-
+//Display the sample metadata, i.e., an individual's demographic information.
 //metadata
 //input.metadata for metadata
 //how to write meta within a function with the plots?
             var metadata= input.metadata
-            console.log(metadata)
+            console.log(metadata);
             //filter??
             //empty info each time
             metadata.html("");
-            console.log(metadata)
-
-            // Object.entries(input).forEach(([key,value]){
-            // var row = metadata.append("");
-            // row.text("${key}:${value}")
+//Display each key-value pair from the metadata JSON object somewhere on the page.
+//Update all of the plots any time that a new sample is selected.
+//make function instead of arrow
+//dropdown=seldtst
+            Object.entries(input).forEach(function([key, values]){
+            var row = metadata.append("");
+            row.text("${key}:${value}");
         
-            
+            d3.select("#selDataset").on("click", input)
+     
+            }
+            )
+            }
 
 
-              
-        }
-    )
-    
-}
 // Add event listener for submit button
-//d3.select("#submit").on("click", handleSubmit);
-
-
-//
+//d3.select("#selDataset").on("click", input);
