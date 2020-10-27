ac5027b4c3dd8cadf742086632962156c9226e4b
fati165 <gdonato38@gmail.com>
2020-10-26 18:33:06 -0700

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,15 +1,18 @@
 
 function bacteria(person){
-    d3.event.preventDefault();
+    //d3.event.preventDefault();
 
-    D3.json('../../samples.json').then(
+    d3.json('../../samples.json').then(
         (input)=> {
             var name = input.samples;
             var otu = name.filter(
-                (row)=> {
-                    row.id == person;
-                }
+                row=> 
+                    row.id == person
+                
             );
+            console.log(name);
+            console.log(input);
+            console.log(otu);
             var sample_values = otu[0].sample_values;
             var otu_ids= otu[0].otu_ids;
             var otu_labels= otu[0].otu_labels;
@@ -65,22 +68,43 @@ function bacteria(person){
             var metadata= input.metadata
             console.log(metadata);
             //filter??
+            var meta = metadata.filter(
+                (row)=> {
+                    row.id == person;
+                });
             //empty info each time
-            metadata.html("");
+            var panel= d3.select("#sample-metadata");
+            panel.html("");
 //Display each key-value pair from the metadata JSON object somewhere on the page.
 //Update all of the plots any time that a new sample is selected.
 //make function instead of arrow
 //dropdown=seldtst
-            Object.entries(input).forEach(function([key, values]){
-            var row = metadata.append("");
-            row.text("${key}:${value}");
+            Object.entries(meta[0]).forEach(function([key, values]){
+            var row = panel.append("p");
+            row.text(`${key}:${values}`);
         
-            d3.select("#selDataset").on("click", input)
      
             }
             )
-            }
+            })}
+function init(){
+    var dropdown= d3.select('#selDataset');
+    d3.json('../../samples.json').then(function(data){
+        var names = data.names;
+        names.forEach(function(id){
+            dropdown.append('option').text(id).property('value', id);
+        })
 
+    })
+
+
+};
+function optionchange(inputs){
+    bacteria(inputs);
+
+}   
+init();       
+//d3.select("#selDataset").on("click", optionchange);
 
 // Add event listener for submit button
 //d3.select("#selDataset").on("click", input);
